e3408fcbddefa285642d4a770411593084d8d8d1
Alex Nicholls <anicholls3@gmail.com>
2020-06-01 13:56:51 -0700

    test: Improve snapshot infrastructure and organization (#687)
    * test: Add helper to enable chromatic snapshots
    
    * test(button): Refactor visual testing stories to use CSF
    
    * test(form-field): Refactor visual testing stories to use CSF
    
    * test(pagination): Refactor visual testing stories to use CSF
    
    * test(select): Refactor visual testing stories to use CSF
    
    * test(popup): Refactor visual testing stories to use CSF
    
    * test(toast): Refactor visual testing stories to use CSF
    
    * test(form-field): Rename testing stories into their own section
    
    * test(button): Rename testing stories into their own section
    
    * test: Rename remainder of testing stories into their own section
    
    * fix(form-field): Fix import
    
    * refactor: Add category to create-component script and use CSF
    
    * feat: Add CSF visual testing story to create-component script
    
    * fix(form-field): Remove unused variable
    
    * fix(form-field): Properly export theme stories
    
    * refactor: Use a decorator for enabling snapshots
    
    * feat: Add override to withSnapshotsEnabled so it can be used per story

--- modules/modal/react/stories/stories.tsx
+++ modules/modal/react/stories/stories.tsx
@@ -2,6 +2,7 @@
 import * as React from 'react';
 import {storiesOf} from '@storybook/react';
 import withReadme from 'storybook-readme/with-readme';
+import {Radio, RadioGroup} from '@workday/canvas-kit-react-radio';
 
 import {Button} from '../../../button/react';
 import Modal, {useModal} from '..';
@@ -54,6 +55,92 @@ const UseModalExample = () => {
         <Button onClick={closeModal} variant={Button.Variant.Secondary}>
           Cancel
         </Button>
+      </Modal>
+    </>
+  );
+};
+
+const ModalWithRadioButtons = () => {
+  const {targetProps, modalProps} = useModal();
+
+  return (
+    <>
+      <Button {...targetProps}>With Radio Buttons</Button>
+      <Modal data-testid="TestModal" heading={'Delete Item'} {...modalProps}>
+        <p>Are you sure you'd like to delete the item titled 'My Item'?</p>
+        <fieldset data-automation-id="ratingFieldset">
+          <legend>Rate this content out of 5 stars</legend>
+          <div>
+            <input
+              id="ratingRadioInput_starsButtonForModal_1"
+              name="ratingRadioInput_starsButtonForModal"
+              type="radio"
+              className="css-1s1qvxb-SingleStarButton"
+              value="1"
+            />
+            <label htmlFor="ratingRadioInput_starsButtonForModal_1">
+              <span className="css-1s1qvxb-SingleStarButton">1 Star</span>
+              <span aria-hidden="true" focusable="false" className="css-xwanzm">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  className="wd-icon-star wd-icon"
+                  focusable="false"
+                  role="presentation"
+                  viewBox="0 0 24 24"
+                >
+                  <g fill-rule="evenodd" className="wd-icon-container">
+                    <path
+                      d="M12 16.717l4.23 2.717-1.154-5.084 3.95-3.556-5.176-.436L12 5.68l-1.85 4.678-5.175.436 3.949 3.556-1.154 5.084z"
+                      className="wd-icon-background"
+                    />
+                    <path
+                      fill-rule="nonzero"
+                      d="M12 16.089l3.59 2.306-.978-4.316 3.352-3.019-4.393-.37L12 6.719l-1.57 3.97-4.394.371 3.352 3.02-.979 4.315L12 16.089zm0 1.942l-5.756 3.696c-.234.15-.376.06-.314-.213l1.57-6.92L2.378 9.98c-.209-.188-.154-.36.118-.383l6.723-.566 2.599-6.57c.1-.254.262-.258.364 0l2.6 6.57 6.722.566c.274.024.325.196.118.383L16.5 14.593l1.57 6.921c.061.268-.081.363-.314.213L12 18.031z"
+                      className="wd-icon-fill"
+                    />
+                  </g>
+                </svg>
+              </span>
+            </label>
+          </div>
+          <div>
+            <input
+              id="ratingRadioInput_starsButtonForModal_1"
+              name="ratingRadioInput_starsButtonForModal"
+              type="radio"
+              className="css-1s1qvxb-SingleStarButton"
+              value="2"
+            />
+            <label htmlFor="ratingRadioInput_starsButtonForModal_1">
+              <span className="css-1s1qvxb-SingleStarButton">1 Star</span>
+              <span aria-hidden="true" focusable="false" className="css-xwanzm">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  className="wd-icon-star wd-icon"
+                  focusable="false"
+                  role="presentation"
+                  viewBox="0 0 24 24"
+                >
+                  <g fill-rule="evenodd" className="wd-icon-container">
+                    <path
+                      d="M12 16.717l4.23 2.717-1.154-5.084 3.95-3.556-5.176-.436L12 5.68l-1.85 4.678-5.175.436 3.949 3.556-1.154 5.084z"
+                      className="wd-icon-background"
+                    />
+                    <path
+                      fill-rule="nonzero"
+                      d="M12 16.089l3.59 2.306-.978-4.316 3.352-3.019-4.393-.37L12 6.719l-1.57 3.97-4.394.371 3.352 3.02-.979 4.315L12 16.089zm0 1.942l-5.756 3.696c-.234.15-.376.06-.314-.213l1.57-6.92L2.378 9.98c-.209-.188-.154-.36.118-.383l6.723-.566 2.599-6.57c.1-.254.262-.258.364 0l2.6 6.57 6.722.566c.274.024.325.196.118.383L16.5 14.593l1.57 6.921c.061.268-.081.363-.314.213L12 18.031z"
+                      className="wd-icon-fill"
+                    />
+                  </g>
+                </svg>
+              </span>
+            </label>
+          </div>
+        </fieldset>
       </Modal>
     </>
   );
@@ -120,6 +207,12 @@ storiesOf('Components|Popups/Modal/React', module)
     <div className="story">
       <h1 className="section-label">Modal</h1>
       <DefaultModalExample />
+    </div>
+  ))
+  .add('With Radio buttons', () => (
+    <div className="story">
+      <h1 className="section-label">Modal</h1>
+      <ModalWithRadioButtons />
     </div>
   ))
   .add('With useModal hook', () => (
